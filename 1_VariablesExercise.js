306892a9abc756650afe38d1c233fb300ccc3773
Dari <80991896+darithedev@users.noreply.github.com>
2025-10-06 01:03:24 -0400

    Refactor variable declarations and answers in exercises

--- 1_VariablesExercise.js
+++ 1_VariablesExercise.js
@@ -12,38 +12,54 @@
 // Data Types and Variables
 
 // Exercise 1. Simply declare a variable named 'emptyVariable'
+var emptyVariable;
 
 // Exercise 2. Declare a variable called 'myName'
+var myName;
 
 // Exercise 3. Assign your name as the value for 'myName'
+var myName = Dari;
 
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
+var dreamDestination = "Costa Rica";
 
 // Exercise 5. Make the following variables and assignments
 // - a variable called num1 with a value greater than 1 and less than 10
 // - a variable called num2 with a value of greater than or equal to 1 and less
 // than or equal to 10
+var num1 = 5;
+var num2 = 10;
 
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for
 // num2 but not num1?
-//
+
+// EXPLAINATION: The difference between the rules for num1 and num2 from the exersice is that num2 can be equal to 1 or 10 while num1 cannot. (<=, >= vs <, >)
+// The valdid values for num2 is 1 - 10, while num1 is 2 - 9 because num2 can be >= 1 and <= 10, meaning a number between and including 1 and 10. 
 
 // Exercise 7. Now we will try some addition.
 // a. Assign the values of 4 and 6 to num1 and num2, respectively.
 // b. Make a new variable called 'theSum', and use 'num1' and
 // 'num2' to assign its value using the "+" operator.
+var num1 = 4;
+var num2 = 6;
+var theSum = num1 + num2;
 
 // Exercise 8. Now we will try some multiplication.
 // Make a new variable called 'theProduct', multiply num1 and num2 and assign
 // the resulting value to 'theProduct'.
+var theProduct = num1 * num2;
 
 // Exercise 9.
 // Make a new variable called 'statement' and using 'myName' and
 // 'dreamDestination' and the concatenation method of your choice, make the
 // value of statement to be:
 // "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
+var statement = "Hi, my name is " + myName + ", and I can't wait to visit " + dreamDestination + "!";
+
+var hi = "Hi, my name is ";
+var statement = hi.concat(myName, ", and I can't wair to visit ", dreamDestination, "!");
 
 // *NOTE* For the following exercises, put your answers in the line below
 // the description of each exercise. For example, below a prompt, you may see:
@@ -54,17 +70,17 @@
 // Exercise 10. What is the data type of the value stored in 'dreamDestination',
 // place your answer in the quotes below:
 
-const myAnswerForExercise10 = "";
+const myAnswerForExercise10 = "string";
 
 // Exercise 11. What is the data type of the value stored in 'product',
 // place your answer in the quotes below:
 
-const myAnswerForExercise11 = "";
+const myAnswerForExercise11 = "number";
 
 // Exercise 12. What do you think the value of emptyVariable is?
 // Place your answer below:
 
-const myAnswerForExercise12 = "";
+const myAnswerForExercise12 = "string"; // if var emptyVariable = ""; otherwise undefine if var emptyVariable;
 
 // Exercise 13. If
 const A = "R";
@@ -73,41 +89,42 @@ const C = 4;
 const D = "D";
 
 // What is the value of B + C
-const myAnswerForExercise13 = "";
+const myAnswerForExercise13 = "5";
 
 // Exercise 14 What is the value of
 // A + "n" + D
-const myAnswerForExercise14 = "";
+const myAnswerForExercise14 = "RnD";
 
 // Exercise 15 What is the value of
 // A + B * 2 + D + C / 2:
-const myAnswerForExercise15 = "";
+const myAnswerForExercise15 = "R2D2";
 
 // Exercise 16 What is the value of A * B
-const myAnswerForExercise16 = "";
+const myAnswerForExercise16 = "R"; // Wrong, console log show NaN
 
 // Exercise 17 What is the value of true || false
-const myAnswerForExercise17 = "";
+const myAnswerForExercise17 = "true"; // tricky 
 
 // Exercise 18 What is the value of 1 === "1"
-const myAnswerForExercise18 = "";
+const myAnswerForExercise18 = "false";
 
 // Exercise 19 What is the value of 1 == "1"
-const myAnswerForExercise19 = "";
+const myAnswerForExercise19 = "true";
 
 // Exercise 20 What is the value of 1 < 1
-const myAnswerForExercise20 = "";
+const myAnswerForExercise20 = "false";
 
 // Exercise 21 What is the value of 1 < 2 < 3
-const myAnswerForExercise21 = "";
+const myAnswerForExercise21 = "true";
 
 // Exercise 22 What is the value of 2 < 1 < 3
-const myAnswerForExercise22 = "";
+const myAnswerForExercise22 = "false"; // true
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
-//
+
+// EXPLAINATION: I found this fairly easy. Though exersizes 16-22 were tricky for me. I used the console to check if my answers were correct. 
 
 // Email your file to the course staff,
 // or commit your file to GitHub and email us a link.
